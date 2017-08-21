fe9a7452ac684489c2961444e22afd3f0545d607
Linda Werner <ischi@Lindas-iMac.fritz.box>
2017-08-21 21:03:58 +0200

    Revert "fix: If the image has the same height and width and you do nothing except rotate the image by 90°, the rotation is not saved."
    This reverts commit 04e051bf028be21ef070a6b4e88a306cb907aa6e.

--- ucrop/src/main/java/com/yalantis/ucrop/task/BitmapCropTask.java
+++ ucrop/src/main/java/com/yalantis/ucrop/task/BitmapCropTask.java
@@ -138,7 +138,7 @@ public class BitmapCropTask extends AsyncTask<Void, Void, Throwable> {
         boolean shouldCrop = shouldCrop(mCroppedImageWidth, mCroppedImageHeight);
         Log.i(TAG, "Should crop: " + shouldCrop);
 
-        if (shouldCrop || mCurrentAngle != 0) {
+        if (shouldCrop) {
             boolean cropped = cropCImg(mImageInputPath, mImageOutputPath,
                     cropOffsetX, cropOffsetY, mCroppedImageWidth, mCroppedImageHeight,
                     mCurrentAngle, resizeScale, mCompressFormat.ordinal(), mCompressQuality,
